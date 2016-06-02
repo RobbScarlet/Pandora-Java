0ce6ca45d0018120b57e791ccee6402ca29dccf1
Remilia <HinanawiCirno@outlook.com>
2016-06-02 17:12:48 +0800

    modify

---
+++ src/main/java/com/robblibrary/pandora/Test.java
@@ -0,0 +1,13 @@
+package com.robblibrary.pandora;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2016-05-30
+ */
+public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+}
---
+++ src/main/java/com/robblibrary/pandora/network/NetworkTest.java
@@ -0,0 +1,17 @@
+package com.robblibrary.pandora.network;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Desc
+ * @Author Remilia
+ * @Create 2016-05-31
+ */
+public class NetworkTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NetworkTest.class);
+
+    public static void main(String[] args){
+
+    }
+}
